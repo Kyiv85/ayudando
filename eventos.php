58cf387233e05d6020d04dde7ad25f1c155ea8cd
Daniel Delgado <dedd712@gmail.com>
2018-03-30 15:21:11 -0400

    Nueva ventana de eventos y backup BD

--- eventos.php
+++ eventos.php
@@ -18,7 +18,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="intro-message">
-                        <h1>Registrate</h1>
+                        <h1>Eventos</h1>
                     </div>
                 </div>
             </div>
@@ -35,76 +35,13 @@
           <div class="col-lg-12 col-sm-12">
             <hr class="section-heading-spacer">
             <div class="clearfix"></div>
-            <h2 class="section-heading">Ponencia sobre búsqueda laboral</h2>
-            <p class="lead">Para el domingo 8 de abirl estaremos realizando un seminario sobre herramientas de bśuqueda laboral, de manera que sirva de apoyo a los compatriotas que aún no consigan trabajo y pueden aprender a manejarse de una manera más eficiente en Argentina a la hora de conseguir empleo. La misma estára siendo de 8am a 1pm.</p>
-            <p class="lead">Vamos a estar llevando un registro y seleccionaremos un grupo de 30 personas entre distintos perfiles que puedan estar asistiendo a la ponencia. Si estás interesado en asitir llena el siguiente formulario y nos pondremos en contacto contigo.</p>
-          </div>
-
-          <div class="col-lg-12 col-sm-12">
-            <hr class="section-heading-spacer">
-              <div class="clearfix"></div>
-                <!-- Start Content Section -->
-                <section id="content">
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-md-9">
-                        <h2>Introduce tus datos</h2>   
-
-                          <!-- Start Contact Form -->
-                          <form role="form" id="contactForm" class="contact-form" data-toggle="validator" class="shake">
-                            <div class="form-group">
-                              <div class="controls">
-                                <input type="text" id="name" class="form-control" placeholder="Nombre" required data-error="Por favor introduce tu nombre">
-                                <div class="help-block with-errors"></div>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <div class="controls">
-                                <input type="text" id="lastname" class="form-control" placeholder="Apellido" required data-error="Por favor introduce tu apellido">
-                                <div class="help-block with-errors"></div>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <div class="controls">
-                                <input type="email" class="email form-control" id="email" placeholder="Email" required data-error="Por favor introduce tu email">
-                                <div class="help-block with-errors"></div>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <div class="controls">
-                                <label for="sel1">Estoy buscando:</label>
-                                <select class="form-control" id="sel1"  required data-error="Observaciones">
-                                  <option>Ayuda</option>
-                                  <option>Prestar ayuda</option>
-                                </select> 
-                                <div class="help-block with-errors"></div>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <div class="controls">
-                                <textarea id="message" rows="7" placeholder="Massage" class="form-control" required data-error="Escribe tus observaciones"></textarea>
-                                <div class="help-block with-errors"></div>
-                              </div>  
-                            </div>
-
-                            <button type="submit" id="submit" class="btn btn-success"></i> Enviar</button>
-                            <button type="clear" id="clear" class="btn btn-warning"></i> Limpiar</button>
-                            <div id="msgSubmit" class="h3 text-center hidden"></div> 
-                            <div class="clearfix"></div>   
-
-                          </form>     
-                          <!-- End Contact Form -->
-
+            <h2 class="section-heading">Seminario perfílate Laboralmente</h2>
+            <div class="text-center">
+              <img src="img/flyer.jpg" class="rounded mx-auto d-block" alt="Flyer">
             </div>
-            <div class="col-md-3">
-              <h2 class="big-title">Información de Contacto</h2>   
-              <div class="information">              
-                <div class="contact-datails">
-                  <p> Gascón 62, Almagro, Buenos Aires, Argentina.</p>
-                  <p> +54 9 11 3255-6087 </p>
-                  <p> dedd712@gmail.com </p>
-                </div>
-              </div>
+            <div class="row" style="margin-top: 30px;">
+              <p class="lead">Para el domingo 8 de abirl estaremos realizando un seminario sobre herramientas de bśuqueda laboral, de manera que sirva de apoyo a los compatriotas que aún no consigan trabajo y pueden aprender a manejarse de una manera más eficiente en Argentina a la hora de conseguir empleo. La misma estára siendo de 8am a 1pm.</p>
+              <p class="lead">Vamos a estar llevando un registro y seleccionaremos un grupo de 30 personas entre distintos perfiles que puedan estar asistiendo a la ponencia. Si estás interesado en asitir regístrate <a href="registro.php">aquí</a> y nos pondremos en contacto contigo.</p>
             </div>
           </div>
         </div>
---
+++ sql/ayudanos.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 30-03-2018 a las 15:53:13
+-- Versión del servidor: 5.5.59-0+deb8u1
+-- Versión de PHP: 5.6.33-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `ayudanos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ayuEventos`
+--
+
+CREATE TABLE IF NOT EXISTS `ayuEventos` (
+`eveCod` int(11) NOT NULL,
+  `eveEstado` enum('A','B') NOT NULL,
+  `eveNombre` varchar(50) NOT NULL,
+  `eveDireccion` varchar(100) NOT NULL,
+  `eveFecha` date NOT NULL,
+  `eveFecAlta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ayuTipoDoc`
+--
+
+CREATE TABLE IF NOT EXISTS `ayuTipoDoc` (
+`tipCod` int(11) NOT NULL,
+  `tipDescripcion` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `ayuTipoDoc`
+--
+
+INSERT INTO `ayuTipoDoc` (`tipCod`, `tipDescripcion`) VALUES
+(1, 'DNI'),
+(2, 'Cédula'),
+(3, 'Pasaporte');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ayuUsuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `ayuUsuarios` (
+`usuCod` int(11) NOT NULL,
+  `usuEstado` enum('A','B') NOT NULL COMMENT 'Estado del usuario (A: Activo, B: Baja)',
+  `usuNombre` varchar(50) NOT NULL,
+  `usuApellido` varchar(50) NOT NULL,
+  `usuEmail` varchar(50) NOT NULL,
+  `tipCod` int(11) NOT NULL,
+  `usuPais` varchar(50) NOT NULL,
+  `usuProvincia` varchar(50) NOT NULL,
+  `usuDireccion` varchar(100) NOT NULL,
+  `usuTipo` enum('N','O','A') NOT NULL COMMENT 'Tipo de Usuario (N: Necesita ayuda, O: Ofrece ayuda, A: Ambos)',
+  `usuProfesion` varchar(50) NOT NULL,
+  `usuFecAlta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ayuUsuariosEventos`
+--
+
+CREATE TABLE IF NOT EXISTS `ayuUsuariosEventos` (
+  `usuCod` int(11) NOT NULL,
+  `eveCod` int(11) NOT NULL,
+  `uevEstado` enum('A','B') NOT NULL,
+  `uevFecAlta` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `ayuEventos`
+--
+ALTER TABLE `ayuEventos`
+ ADD PRIMARY KEY (`eveCod`);
+
+--
+-- Indices de la tabla `ayuTipoDoc`
+--
+ALTER TABLE `ayuTipoDoc`
+ ADD PRIMARY KEY (`tipCod`);
+
+--
+-- Indices de la tabla `ayuUsuarios`
+--
+ALTER TABLE `ayuUsuarios`
+ ADD PRIMARY KEY (`usuCod`), ADD KEY `tipCod` (`tipCod`);
+
+--
+-- Indices de la tabla `ayuUsuariosEventos`
+--
+ALTER TABLE `ayuUsuariosEventos`
+ ADD UNIQUE KEY `usuCod` (`usuCod`,`eveCod`), ADD KEY `fk_cod_evento` (`eveCod`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ayuEventos`
+--
+ALTER TABLE `ayuEventos`
+MODIFY `eveCod` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ayuTipoDoc`
+--
+ALTER TABLE `ayuTipoDoc`
+MODIFY `tipCod` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `ayuUsuarios`
+--
+ALTER TABLE `ayuUsuarios`
+MODIFY `usuCod` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ayuUsuarios`
+--
+ALTER TABLE `ayuUsuarios`
+ADD CONSTRAINT `fk_tipo_doc` FOREIGN KEY (`tipCod`) REFERENCES `ayuTipoDoc` (`tipCod`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ayuUsuariosEventos`
+--
+ALTER TABLE `ayuUsuariosEventos`
+ADD CONSTRAINT `fk_cod_evento` FOREIGN KEY (`eveCod`) REFERENCES `ayuEventos` (`eveCod`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_cod_usuario` FOREIGN KEY (`usuCod`) REFERENCES `ayuUsuarios` (`usuCod`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+/* USUARIO PARA LA BASE DE DATOS */
+CREATE USER 'ayudanos'@'localhost' IDENTIFIED BY 'M4dur0c0#03Tum4dr3';
+GRANT ALL PRIVILEGES ON ayudanos.* TO 'ayudanos'@'localhost';
+FLUSH PRIVILEGES;
