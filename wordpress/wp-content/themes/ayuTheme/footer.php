ca7f2db7bb0bce993a3f5932f99483179f12b0cd
Daniel Delgado <delgadodan@globalhitss.com>
2019-06-03 18:05:50 -0300

    FIX: Creación de theme basado en el existenten y respaldo sql

---
+++ wordpress/wp-content/themes/ayuTheme/footer.php
@@ -0,0 +1,27 @@
+<!-- Footer -->
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+                    <p class="copyright text-muted small">Copyright &copy; By <a href="https://twitter.com/Kyiv85">Kyiv85</a>. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Main JS  -->      
+    <script src="js/form-validator.min.js"></script>  
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/ayudando.js"></script>
+
+
+
+    <?php wp_footer(); //Crucial footer hook! ?>
+</body>
+</html>
