e4504132502df425f37f324b70720f61de2578dd
Daniel Delgado <dedd712@gmail.com>
2020-02-07 00:29:52 -0300

    Ajuste en números elegidos

--- responses/rifa/responseShowNumbers.php
+++ responses/rifa/responseShowNumbers.php
@@ -10,30 +10,34 @@ if (isset($_POST["accion"]) && ($_POST["accion"] == "validateNumbers")){
     }
     else{
         $text = '';
-        $numeros = [];
+        $selectedNumbers = [];
+        $totalNumbers = [];
         foreach ($total as $t){
-            array_push($numeros, $t['rifNumero']);
+            array_push($selectedNumbers, $t['rifNumero']);
         }
         for($i=0;$i<1000;$i++){
+            array_push($totalNumbers, $i);
+        }
+        foreach($totalNumbers as $t){
             $text .= '<div class="col-2">
                           <div class="input-group">
                               <div class="form-check">';
             $existe = false;
-            foreach ($numeros as $n){
-                if($i == $n['rifNumero']){
+            foreach ($selectedNumbers as $s){
+                if($t == $s){
                     $existe = true;
                 }
             }
             if($existe){
-                $text .= '<input class="form-check-input" type="checkbox" value="'.$i.'" id="rifNumero_'.$i.'" disabled>
+                $text .= '<input class="form-check-input" type="checkbox" value="'.$t.'" id="rifNumero_'.$t.'" disabled>
                           <label class="form-check-label" for="defaultCheck2" style="color:red;">
-                            '.str_pad(strval($i), 3, "0", STR_PAD_LEFT).'
+                            '.str_pad(strval($t), 3, "0", STR_PAD_LEFT).'
                           </label>';
             }
             else{
-                $text .= '<input class="form-check-input" type="checkbox" value="'.$i.'" id="rifNumero_'.$i.'">
+                $text .= '<input class="form-check-input" type="checkbox" value="'.$t.'" id="rifNumero_'.$t.'">
                           <label class="form-check-label" for="defaultCheck1" style="margin-left: 25px;">
-                            '.str_pad(strval($i), 3, "0", STR_PAD_LEFT).'
+                            '.str_pad(strval($t), 3, "0", STR_PAD_LEFT).'
                           </label>';
             }
             $text .= '
